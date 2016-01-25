56c7c958a331679a7b9bbd3b5d0e3215ce6f1b83
xiuc <hang.lou@tqmall.com>
2016-01-25 15:27:09 +0800

    格式化python文件

--- SearchLogF.py
+++ SearchLogF.py
@@ -5,17 +5,17 @@ import os.path
 import pdb
 
 fdir = '/Users/xiuc/logs/search_neiwang'
-fout = open('/Users/xiuc/logs/earchResult','a')
+fout = open('/Users/xiuc/logs/earchResult', 'a')
 for i in os.listdir(fdir):
-	if os.path.isfile(os.path.join(fdir,i)):
-		fin = open(os.path.join(fdir,i),'r')
-		done = 0
-		while not done:
-			line = fin.readline()
-			if line != '':
-				if line.find('/goods/lid') >= 0:
-					fout.write(line)
-			else:
-				done = 1
-		fin.close()
+    if os.path.isfile(os.path.join(fdir, i)):
+        fin = open(os.path.join(fdir, i), 'r')
+        done = 0
+        while not done:
+            line = fin.readline()
+            if line != '':
+                if line.find('/goods/lid') >= 0:
+                    fout.write(line)
+            else:
+                done = 1
+        fin.close()
 fout.close()
