4a7c99c52a22c108a0df28c336771658a252edd2
xiuc <hang.lou@tqmall.com>
2016-01-25 15:14:38 +0800

    Search simple log in dir

---
+++ SearchLogF.py
@@ -0,0 +1,21 @@
+#! /usr/bin/env python3
+
+import os
+import os.path
+import pdb
+
+fdir = '/Users/xiuc/logs/search_neiwang'
+fout = open('/Users/xiuc/logs/earchResult','a')
+for i in os.listdir(fdir):
+	if os.path.isfile(os.path.join(fdir,i)):
+		fin = open(os.path.join(fdir,i),'r')
+		done = 0
+		while not done:
+			line = fin.readline()
+			if line != '':
+				if line.find('/goods/lid') >= 0:
+					fout.write(line)
+			else:
+				done = 1
+		fin.close()
+fout.close()
