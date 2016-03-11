6800d01b8bd3dec8aadfc2badf96dc947461a1cf
xiuc <972994718@qq.com>
2016-03-11 17:46:06 +0800

    search log

--- scripts/SearchLogF.py
+++ scripts/SearchLogF.py
@@ -4,8 +4,8 @@ import os
 import os.path
 import pdb
 
-fdir = '/Users/xiuc/logs/search_neiwang'
-fout = open('/Users/xiuc/logs/earchResult', 'a')
+fdir = '/Users/xiuc/tmp/201603/localhost_access_log_23'
+fout = open('/Users/xiuc/tmp/201603/resultOrder', 'a')
 for i in os.listdir(fdir):
     if os.path.isfile(os.path.join(fdir, i)):
         fin = open(os.path.join(fdir, i), 'r')
@@ -13,8 +13,8 @@ for i in os.listdir(fdir):
         while not done:
             line = fin.readline()
             if line != '':
-                if line.find('/goods/car_parts_suggest') >= 0:
-                    fout.write(line)
+                #if line.find('/order/') >= 0:
+                fout.write(line)
             else:
                 done = 1
         fin.close()
