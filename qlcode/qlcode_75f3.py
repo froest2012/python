37c0659c197a614036569bf30b29bb3c7feab402
xiuc <972994718@qq.com>
2016-03-14 23:20:44 +0800

    improve the pack01

--- qlcode/qlcode_75f3.py
+++ qlcode/qlcode_75f3.py
@@ -7,33 +7,13 @@ import pdb
 
 v = 5000
 n = 15
-arr = [509,838,924,650,604,793,564,651,697,649,747,787,701,605,644]
-f = [[0] * v for i in range(n+1)]
-dic = {}
-b = 0
-ii = 0
-jj = 0
-for i in range(1,n+1):
-	for j in range(v):
-		if j >= arr[i-1] and f[i-1][j] < f[i-1][j-arr[i-1]]+arr[i-1]:
-			f[i][j] = f[i-1][j-arr[i-1]]+arr[i-1]
-			ijl=[]
-			ijl.append(i)
-			ijl.append(j)
-			dic[f[i][j]] = ijl
-			if j == 4999:
-				b = f[i][j]
-				ii = i
-				jj = j
-				#print("%d" %(i-1),end='-')
-		else:
-			f[i][j] = f[i-1][j]
-
-while b > 0:
-	print(ii,end="-")
-#	pdb.set_trace()
-	b = f[ii][jj]-arr[ii-1]
-	if b != 0:
-		ii = dic[b][0]
-		jj = dic[b][1]
-
+arr = [0,509,838,924,650,604,793,564,651,697,649,747,787,701,605,644]
+f = [[0] * (v + 1) for i in range(n + 1)]
+def zero012(f, cost, weight, v):
+	l = len(cost)
+	for i in range(1,l):
+		for j in range(cost[i], v + 1)[::-1]:
+			if i >= 1 and j >= cost[i]:
+				f[i][j] = max(f[i - 1][j],f[i - 1][j - cost[i]] + weight[i])
+	print(f[l - 1][v])
+zero012(f, arr, arr, v)
