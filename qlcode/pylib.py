4468cef5d4016506f654ac13b34b9141490588e0
xiuc <972994718@qq.com>
2016-03-16 23:16:01 +0800

    pack01

--- qlcode/pylib.py
+++ qlcode/pylib.py
@@ -4,7 +4,7 @@
 import pdb
 
 # 01背包问题============================================================
-cost = [509, 838, 924, 650, 604, 793, 564, 651, 697, 649, 747, 787, 701, 605, 644]
+cost = [0, 509, 838, 924, 650, 604, 793, 564, 651, 697, 649, 747, 787, 701, 605, 644]
 v = 5000
 
 
@@ -12,18 +12,28 @@ v = 5000
 # weight = [4, 6, 12, 7]
 # v = 6
 
-
-def zeroOnePack0(cost, weight, v):
-	"""01背包问题，未经过优化，时间O(NV)，空间f[n][v];ß"""
-	n = len(cost)
-	f = [[0] * v for i in range(n)]
-	for i in range(n):
-		for j in range(v):
-			if j >= cost[i] and i > 0:
+# 01背包
+# 使用二维数组来存储状态, 此时可以算出01背包的物品选择方案
+# f[i][j] = max{f[i - 1][j],f[i - 1][j - cost[i]] + weight[i]}
+def zero012(f, cost, weight, v):
+	l = len(cost)
+	for i in range(1, l):
+		for j in range(v + 1):
+			if j >= cost[i]:
 				f[i][j] = max(f[i - 1][j], f[i - 1][j - cost[i]] + weight[i])
 			else:
 				f[i][j] = f[i - 1][j]
-	print(f[3][5])
+	print(f[l - 1][v])
+
+# 01背包使用二维数组算出物品选择方案
+def prin(f, cost, weight, i, j, l):
+	if i <= 0:
+		return
+	if f[i][j] == f[i - 1][j - cost[i]] + weight[i]:
+		l.append(i)
+		prin(f, cost, weight, i - 1, j - cost[i], l)
+	else:
+		prin(f, cost, weight, i - 1, j, l)
 
 
 # 01背包
@@ -35,7 +45,7 @@ def zeroOnePack0(cost, weight, v):
 def pack01(cost, weight, v, sum):
 	n = len(cost)
 	f = [0 for i in range(v + 1)]
-	for i in range(n):
+	for i in range(1, n):
 		b = max(v - sum, cost[i])
 		for j in range(b, v + 1)[::-1]:
 			f[j] = max(f[j], f[j - cost[i]] + weight[i])
--- qlcode/qlcode_75f3.py
+++ qlcode/qlcode_75f3.py
@@ -1,19 +1,38 @@
 #! /usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-#超级丑，全世界没有之一
-#动态规划解01背包问题，计算路径，最大收益
+# 超级丑，全世界没有之一
+# 动态规划解01背包问题，计算路径，最大收益
 import pdb
 
 v = 5000
 n = 15
-arr = [0,509,838,924,650,604,793,564,651,697,649,747,787,701,605,644]
+arr = [0, 509, 838, 924, 650, 604, 793, 564, 651, 697, 649, 747, 787, 701, 605, 644]
 f = [[0] * (v + 1) for i in range(n + 1)]
+
+
 def zero012(f, cost, weight, v):
 	l = len(cost)
-	for i in range(1,l):
-		for j in range(cost[i], v + 1)[::-1]:
-			if i >= 1 and j >= cost[i]:
-				f[i][j] = max(f[i - 1][j],f[i - 1][j - cost[i]] + weight[i])
+	for i in range(1, l):
+		for j in range(v + 1):
+			if j >= cost[i]:
+				f[i][j] = max(f[i - 1][j], f[i - 1][j - cost[i]] + weight[i])
+			else:
+				f[i][j] = f[i - 1][j]
 	print(f[l - 1][v])
+
+
+def prin(f, cost, weight, i, j, l):
+	if i <= 0:
+		return
+	if f[i][j] == f[i - 1][j - cost[i]] + weight[i]:
+		l.append(i)
+		prin(f, cost, weight, i - 1, j - cost[i], l)
+	else:
+		prin(f, cost, weight, i - 1, j, l)
+
+
 zero012(f, arr, arr, v)
+l = []
+prin(f, arr, arr, n, v, l)
+print(l)
